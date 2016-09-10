5f89cf0edcf3af359979b813c8656be2ef180230
XuiKoh <silversui@hotmail.com>
2016-09-10 15:43:27 +1000

    1st implement Mvvm

--- Resources/Resource.Designer.cs
+++ Resources/Resource.Designer.cs
@@ -26,10 +26,52 @@ namespace Spoteam_App
 		
 		public static void UpdateIdValues()
 		{
+			global::MvvmCross.Binding.Droid.Resource.Attribute.MvxBind = global::Spoteam_App.Resource.Attribute.MvxBind;
+			global::MvvmCross.Binding.Droid.Resource.Attribute.MvxDropDownItemTemplate = global::Spoteam_App.Resource.Attribute.MvxDropDownItemTemplate;
+			global::MvvmCross.Binding.Droid.Resource.Attribute.MvxGroupItemTemplate = global::Spoteam_App.Resource.Attribute.MvxGroupItemTemplate;
+			global::MvvmCross.Binding.Droid.Resource.Attribute.MvxItemTemplate = global::Spoteam_App.Resource.Attribute.MvxItemTemplate;
+			global::MvvmCross.Binding.Droid.Resource.Attribute.MvxLang = global::Spoteam_App.Resource.Attribute.MvxLang;
+			global::MvvmCross.Binding.Droid.Resource.Attribute.MvxSource = global::Spoteam_App.Resource.Attribute.MvxSource;
+			global::MvvmCross.Binding.Droid.Resource.Attribute.MvxTemplate = global::Spoteam_App.Resource.Attribute.MvxTemplate;
+			global::MvvmCross.Binding.Droid.Resource.Id.MvvmCrossTagId = global::Spoteam_App.Resource.Id.MvvmCrossTagId;
+			global::MvvmCross.Binding.Droid.Resource.Id.MvxBindingTagUnique = global::Spoteam_App.Resource.Id.MvxBindingTagUnique;
+			global::MvvmCross.Binding.Droid.Resource.Styleable.MvxBinding = global::Spoteam_App.Resource.Styleable.MvxBinding;
+			global::MvvmCross.Binding.Droid.Resource.Styleable.MvxBinding_MvxBind = global::Spoteam_App.Resource.Styleable.MvxBinding_MvxBind;
+			global::MvvmCross.Binding.Droid.Resource.Styleable.MvxBinding_MvxLang = global::Spoteam_App.Resource.Styleable.MvxBinding_MvxLang;
+			global::MvvmCross.Binding.Droid.Resource.Styleable.MvxControl = global::Spoteam_App.Resource.Styleable.MvxControl;
+			global::MvvmCross.Binding.Droid.Resource.Styleable.MvxControl_MvxTemplate = global::Spoteam_App.Resource.Styleable.MvxControl_MvxTemplate;
+			global::MvvmCross.Binding.Droid.Resource.Styleable.MvxExpandableListView = global::Spoteam_App.Resource.Styleable.MvxExpandableListView;
+			global::MvvmCross.Binding.Droid.Resource.Styleable.MvxExpandableListView_MvxGroupItemTemplate = global::Spoteam_App.Resource.Styleable.MvxExpandableListView_MvxGroupItemTemplate;
+			global::MvvmCross.Binding.Droid.Resource.Styleable.MvxImageView = global::Spoteam_App.Resource.Styleable.MvxImageView;
+			global::MvvmCross.Binding.Droid.Resource.Styleable.MvxImageView_MvxSource = global::Spoteam_App.Resource.Styleable.MvxImageView_MvxSource;
+			global::MvvmCross.Binding.Droid.Resource.Styleable.MvxListView = global::Spoteam_App.Resource.Styleable.MvxListView;
+			global::MvvmCross.Binding.Droid.Resource.Styleable.MvxListView_MvxDropDownItemTemplate = global::Spoteam_App.Resource.Styleable.MvxListView_MvxDropDownItemTemplate;
+			global::MvvmCross.Binding.Droid.Resource.Styleable.MvxListView_MvxItemTemplate = global::Spoteam_App.Resource.Styleable.MvxListView_MvxItemTemplate;
 		}
 		
 		public partial class Attribute
 		{
+			
+			// aapt resource value: 0x7f010000
+			public const int MvxBind = 2130771968;
+			
+			// aapt resource value: 0x7f010004
+			public const int MvxDropDownItemTemplate = 2130771972;
+			
+			// aapt resource value: 0x7f010005
+			public const int MvxGroupItemTemplate = 2130771973;
+			
+			// aapt resource value: 0x7f010003
+			public const int MvxItemTemplate = 2130771971;
+			
+			// aapt resource value: 0x7f010001
+			public const int MvxLang = 2130771969;
+			
+			// aapt resource value: 0x7f010006
+			public const int MvxSource = 2130771974;
+			
+			// aapt resource value: 0x7f010002
+			public const int MvxTemplate = 2130771970;
 			
 			static Attribute()
 			{
@@ -60,16 +102,19 @@ namespace Spoteam_App
 			public const int og = 2130837508;
 			
 			// aapt resource value: 0x7f020005
-			public const int st = 2130837509;
+			public const int splash = 2130837509;
 			
 			// aapt resource value: 0x7f020006
-			public const int wil = 2130837510;
+			public const int st = 2130837510;
 			
 			// aapt resource value: 0x7f020007
-			public const int Xui = 2130837511;
+			public const int wil = 2130837511;
 			
 			// aapt resource value: 0x7f020008
-			public const int zg = 2130837512;
+			public const int Xui = 2130837512;
+			
+			// aapt resource value: 0x7f020009
+			public const int zg = 2130837513;
 			
 			static Drawable()
 			{
@@ -84,95 +129,101 @@ namespace Spoteam_App
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050015
-			public const int btn_accept = 2131034133;
+			// aapt resource value: 0x7f040000
+			public const int MvvmCrossTagId = 2130968576;
 			
-			// aapt resource value: 0x7f05001d
-			public const int btn_back = 2131034141;
+			// aapt resource value: 0x7f040001
+			public const int MvxBindingTagUnique = 2130968577;
 			
-			// aapt resource value: 0x7f050013
-			public const int btn_deny = 2131034131;
+			// aapt resource value: 0x7f040017
+			public const int btn_accept = 2130968599;
 			
-			// aapt resource value: 0x7f05000f
-			public const int btn_group = 2131034127;
+			// aapt resource value: 0x7f04001f
+			public const int btn_back = 2130968607;
 			
-			// aapt resource value: 0x7f050010
-			public const int btn_individual = 2131034128;
+			// aapt resource value: 0x7f040015
+			public const int btn_deny = 2130968597;
 			
-			// aapt resource value: 0x7f050011
-			public const int btn_profile = 2131034129;
+			// aapt resource value: 0x7f040011
+			public const int btn_group = 2130968593;
 			
-			// aapt resource value: 0x7f050004
-			public const int btn_tryagain = 2131034116;
+			// aapt resource value: 0x7f040012
+			public const int btn_individual = 2130968594;
 			
-			// aapt resource value: 0x7f050008
-			public const int group_button1 = 2131034120;
+			// aapt resource value: 0x7f040013
+			public const int btn_profile = 2130968595;
 			
-			// aapt resource value: 0x7f050000
-			public const int imageView1 = 2131034112;
+			// aapt resource value: 0x7f040006
+			public const int btn_tryagain = 2130968582;
 			
-			// aapt resource value: 0x7f05001c
-			public const int imageView2 = 2131034140;
+			// aapt resource value: 0x7f04000a
+			public const int group_button1 = 2130968586;
 			
-			// aapt resource value: 0x7f050012
-			public const int linearLayout1 = 2131034130;
+			// aapt resource value: 0x7f040002
+			public const int imageView1 = 2130968578;
 			
-			// aapt resource value: 0x7f05000e
-			public const int linearLayout2 = 2131034126;
+			// aapt resource value: 0x7f04001e
+			public const int imageView2 = 2130968606;
 			
-			// aapt resource value: 0x7f050016
-			public const int linearLayout3 = 2131034134;
+			// aapt resource value: 0x7f040014
+			public const int linearLayout1 = 2130968596;
 			
-			// aapt resource value: 0x7f05000a
-			public const int location_button1 = 2131034122;
+			// aapt resource value: 0x7f040010
+			public const int linearLayout2 = 2130968592;
 			
-			// aapt resource value: 0x7f050018
-			public const int radioButton1 = 2131034136;
+			// aapt resource value: 0x7f040018
+			public const int linearLayout3 = 2130968600;
 			
-			// aapt resource value: 0x7f050019
-			public const int radioButton2 = 2131034137;
+			// aapt resource value: 0x7f04000c
+			public const int location_button1 = 2130968588;
 			
-			// aapt resource value: 0x7f05001a
-			public const int radioButton3 = 2131034138;
+			// aapt resource value: 0x7f04001a
+			public const int radioButton1 = 2130968602;
 			
-			// aapt resource value: 0x7f05001b
-			public const int radioButton4 = 2131034139;
+			// aapt resource value: 0x7f04001b
+			public const int radioButton2 = 2130968603;
 			
-			// aapt resource value: 0x7f050017
-			public const int radioGroup1 = 2131034135;
+			// aapt resource value: 0x7f04001c
+			public const int radioButton3 = 2130968604;
 			
-			// aapt resource value: 0x7f050014
-			public const int space1 = 2131034132;
+			// aapt resource value: 0x7f04001d
+			public const int radioButton4 = 2130968605;
 			
-			// aapt resource value: 0x7f050005
-			public const int tableLayout1 = 2131034117;
+			// aapt resource value: 0x7f040019
+			public const int radioGroup1 = 2130968601;
 			
-			// aapt resource value: 0x7f050006
-			public const int tableRow1 = 2131034118;
+			// aapt resource value: 0x7f040016
+			public const int space1 = 2130968598;
 			
-			// aapt resource value: 0x7f050009
-			public const int tableRow2 = 2131034121;
+			// aapt resource value: 0x7f040007
+			public const int tableLayout1 = 2130968583;
 			
-			// aapt resource value: 0x7f05000b
-			public const int tableRow3 = 2131034123;
+			// aapt resource value: 0x7f040008
+			public const int tableRow1 = 2130968584;
 			
-			// aapt resource value: 0x7f05000c
-			public const int tableRow4 = 2131034124;
+			// aapt resource value: 0x7f04000b
+			public const int tableRow2 = 2130968587;
 			
-			// aapt resource value: 0x7f050001
-			public const int textView1 = 2131034113;
+			// aapt resource value: 0x7f04000d
+			public const int tableRow3 = 2130968589;
 			
-			// aapt resource value: 0x7f050002
-			public const int textView2 = 2131034114;
+			// aapt resource value: 0x7f04000e
+			public const int tableRow4 = 2130968590;
 			
-			// aapt resource value: 0x7f050003
-			public const int textView3 = 2131034115;
+			// aapt resource value: 0x7f040003
+			public const int textView1 = 2130968579;
 			
-			// aapt resource value: 0x7f05000d
-			public const int textView4 = 2131034125;
+			// aapt resource value: 0x7f040004
+			public const int textView2 = 2130968580;
 			
-			// aapt resource value: 0x7f050007
-			public const int username_text = 2131034119;
+			// aapt resource value: 0x7f040005
+			public const int textView3 = 2130968581;
+			
+			// aapt resource value: 0x7f04000f
+			public const int textView4 = 2130968591;
+			
+			// aapt resource value: 0x7f040009
+			public const int username_text = 2130968585;
 			
 			static Id()
 			{
@@ -191,19 +242,25 @@ namespace Spoteam_App
 			public const int Busy = 2130903040;
 			
 			// aapt resource value: 0x7f030001
-			public const int Group = 2130903041;
+			public const int FirstView = 2130903041;
 			
 			// aapt resource value: 0x7f030002
-			public const int LocationRequest = 2130903042;
+			public const int Group = 2130903042;
 			
 			// aapt resource value: 0x7f030003
-			public const int Main = 2130903043;
+			public const int LocationRequest = 2130903043;
 			
 			// aapt resource value: 0x7f030004
-			public const int ProfileSetting = 2130903044;
+			public const int Main = 2130903044;
 			
 			// aapt resource value: 0x7f030005
-			public const int RequestAccepted = 2130903045;
+			public const int ProfileSetting = 2130903045;
+			
+			// aapt resource value: 0x7f030006
+			public const int RequestAccepted = 2130903046;
+			
+			// aapt resource value: 0x7f030007
+			public const int SplashScreen = 2130903047;
 			
 			static Layout()
 			{
@@ -218,11 +275,11 @@ namespace Spoteam_App
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f040001
-			public const int ApplicationName = 2130968577;
+			// aapt resource value: 0x7f060001
+			public const int ApplicationName = 2131099649;
 			
-			// aapt resource value: 0x7f040000
-			public const int Hello = 2130968576;
+			// aapt resource value: 0x7f060000
+			public const int Hello = 2131099648;
 			
 			static String()
 			{
@@ -230,6 +287,73 @@ namespace Spoteam_App
 			}
 			
 			private String()
+			{
+			}
+		}
+		
+		public partial class Style
+		{
+			
+			// aapt resource value: 0x7f050000
+			public const int Theme_Splash = 2131034112;
+			
+			static Style()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Style()
+			{
+			}
+		}
+		
+		public partial class Styleable
+		{
+			
+			public static int[] MvxBinding = new int[] {
+					2130771968,
+					2130771969};
+			
+			// aapt resource value: 0
+			public const int MvxBinding_MvxBind = 0;
+			
+			// aapt resource value: 1
+			public const int MvxBinding_MvxLang = 1;
+			
+			public static int[] MvxControl = new int[] {
+					2130771970};
+			
+			// aapt resource value: 0
+			public const int MvxControl_MvxTemplate = 0;
+			
+			public static int[] MvxExpandableListView = new int[] {
+					2130771973};
+			
+			// aapt resource value: 0
+			public const int MvxExpandableListView_MvxGroupItemTemplate = 0;
+			
+			public static int[] MvxImageView = new int[] {
+					2130771974};
+			
+			// aapt resource value: 0
+			public const int MvxImageView_MvxSource = 0;
+			
+			public static int[] MvxListView = new int[] {
+					2130771971,
+					2130771972};
+			
+			// aapt resource value: 1
+			public const int MvxListView_MvxDropDownItemTemplate = 1;
+			
+			// aapt resource value: 0
+			public const int MvxListView_MvxItemTemplate = 0;
+			
+			static Styleable()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Styleable()
 			{
 			}
 		}
