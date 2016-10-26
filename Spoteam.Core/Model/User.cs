38d22eec237fd7b5faac366cf8e6e8d61054c4ce
XuiKoh <silversui@hotmail.com>
2016-10-26 18:37:09 +1000

    Puting CoreFolder Backt ogether

---
+++ Spoteam.Core/Model/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Spoteam.Core.Models
+{
+    public class User
+    {
+        public string UserLocation { get; set; }
+        public string UserName { get; set; }
+        public int UserStatus { get; set; }
+        public string UserIcon { get; set; }
+
+        public User() { }
+        public User(string userName, string unitLocation, string usericon)
+        {
+            UserName = userName;
+            UserLocation = unitLocation;
+            UserIcon = usericon;
+            
+        }
+
+    }
+}
