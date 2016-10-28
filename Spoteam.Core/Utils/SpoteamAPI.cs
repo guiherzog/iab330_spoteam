1b1b3961253daa5695b5c72fd81056248e4e7f6f
Stellar Tech MacOS <macos@stellartechnology.com.au>
2016-10-28 20:19:08 +1000

    - Fixed merge errors;

--- Spoteam.Core/Utils/SpoteamAPI.cs
+++ Spoteam.Core/Utils/SpoteamAPI.cs
@@ -1,40 +1,43 @@
 ﻿using Newtonsoft.Json;
 using Spoteam.Core.Model;
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
-namespace Spoteam.Core.Utils {
-    class SpoteamAPI{
-        HttpClient client;
-        private const string server = "http://13.70.86.190:8128";
+namespace Spoteam.Core.Utils
+{
+	class SpoteamAPI
+	{
+		HttpClient client;
+		private const string server = "http://13.70.86.190:8128";
 
-        public SpoteamAPI() {
-            client = new HttpClient();
-            client.MaxResponseContentBufferSize = 256000;
-        }
+		public SpoteamAPI()
+		{
+			client = new HttpClient();
+			client.MaxResponseContentBufferSize = 256000;
+		}
 
-        public async Task<GetUserResult> Get(string table, string column, string value) {
-            string URL = String.Format("{0}/get/{1}/{2}?value={3}", server, table, column, value);
-            var uri = new Uri(URL);
-            var response = await client.GetAsync(uri);
-            if (response.IsSuccessStatusCode) {
-                string content = await response.Content.ReadAsStringAsync();
-                switch (table) {
-                    case "user":
-                        return JsonConvert.DeserializeObject<GetUserResult>(content);
-                    case "location":
-                    case "team":
-                    default:
-                        return null;
-                }
-            } else {
-                return null;
-            }
-        }
-    }
+		public async Task<GetUserResult> Get(string table, string column, string value)
+		{
+			string URL = String.Format("{0}/get/{1}/{2}?value={3}", server, table, column, value);
+			var uri = new Uri(URL);
+			var response = await client.GetAsync(uri);
+			if (response.IsSuccessStatusCode)
+			{
+				string content = await response.Content.ReadAsStringAsync();
+				switch (table)
+				{
+					case "user":
+						return JsonConvert.DeserializeObject<GetUserResult>(content);
+					case "location":
+					case "team":
+					default:
+						return null;
+				}
+			}
+			else {
+				return null;
+			}
+		}
+	}
 }
--- Spoteam.Core/ViewModels/SecondViewModel.cs
+++ Spoteam.Core/ViewModels/SecondViewModel.cs
@@ -3,63 +3,60 @@ using Spoteam.Core.Model;
 using Spoteam.Core.Models;
 using Spoteam.Core.Utils;
 //using MvvmCrossDemo.Core.Services;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Spoteam.Core.ViewModels
 {
-    public class SecondViewModel : MvxViewModel
-    {
-        private User selectedUser;
+	public class SecondViewModel : MvxViewModel
+	{
+		private User selectedUser;
 
-        private string selectedusername;
+		private string selectedusername;
 
-        private SpoteamAPI spoteamAPI = new SpoteamAPI();
-        
+		private SpoteamAPI spoteamAPI = new SpoteamAPI();
 
-        public string UserName
-        {
-            get { return selectedusername; }
-            set { SetProperty(ref selectedusername, value); }
-        }
 
-        private string selecteduserlocation;
+		public string UserName
+		{
+			get { return selectedusername; }
+			set { SetProperty(ref selectedusername, value); }
+		}
 
-        public string UserLocation
-        {
-            get { return selecteduserlocation; }
-            set { SetProperty(ref selecteduserlocation, value); }
-        }
-        private string selectedusericon;
+		private string selecteduserlocation;
 
-        public string UserIcon
-        {
-            get { return selectedusericon; }
-            set { SetProperty(ref selectedusericon, value); }
-        }
+		public string UserLocation
+		{
+			get { return selecteduserlocation; }
+			set { SetProperty(ref selecteduserlocation, value); }
+		}
+		private string selectedusericon;
 
-        public void Init(User parameters)
-        {
-            selectedUser = parameters;
-        }
-        public override void Start()
-        {
-            base.Start();
-            UserName = selectedUser.name;
-            UserLocation = selectedUser.location.ToString();
-            UserIcon = selectedUser.image;
-            GetUsers();
-        }
+		public string UserIcon
+		{
+			get { return selectedusericon; }
+			set { SetProperty(ref selectedusericon, value); }
+		}
 
-        public async void GetUsers() {
-            GetUserResult result = await spoteamAPI.Get("user", "status", "available");
-            foreach (var row in result.rows) {
-                Debug.WriteLine("User " + row.name + " - Email: " + row.email + " - Image: " + row.image + " - Location: " + row.location + " - Status: " + row.status);
-            }
-        }
-    }
+		public void Init(User parameters)
+		{
+			selectedUser = parameters;
+		}
+		public override void Start()
+		{
+			base.Start();
+			UserName = selectedUser.name;
+			UserLocation = selectedUser.location.ToString();
+			UserIcon = selectedUser.image;
+			GetUsers();
+		}
+
+		public async void GetUsers()
+		{
+			GetUserResult result = await spoteamAPI.Get("user", "status", "available");
+			foreach (var row in result.rows)
+			{
+				Debug.WriteLine("User " + row.name + " - Email: " + row.email + " - Image: " + row.image + " - Location: " + row.location + " - Status: " + row.status);
+			}
+		}
+	}
 }
